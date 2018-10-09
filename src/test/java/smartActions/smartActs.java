fcfea21529e5ddfeec30ed0414cf0c0ae4bef963
yyang212 <yyang212@US53EPJYMT.us.nam.ad.pwcinternal.com>
2018-10-09 12:34:02 +0800

    test

--- src/test/java/smartActions/smartActs.java
+++ src/test/java/smartActions/smartActs.java
@@ -7,7 +7,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class smartActs {
-	WebDriver driver;
+	
+	private WebDriver driver;
 
 	// check page title
 	public void waitPageTitle(String title) {
